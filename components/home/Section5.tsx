6443301e72f36fb1d425bbb3a33129e551559305
AnasMuhammedAK <anasabdulkareem100.com>
2023-01-30 12:41:35 +0530

    ...

--- components/home/Section5.tsx
+++ components/home/Section5.tsx
@@ -7,16 +7,9 @@ function Section5() {
     initAOS();
   }, []);
   return (
-    <div className=" py-16 dark:bg-megamind_black lg:mt-[-80px] ">
-      <div
-        data-aos="zoom-in-up"
-        className=" hidden xl:block text-8xl 2xl:text-[160px] dark:text-megamind_white/5 text-center font-extrabold text-gray-200 z-[-10] px-4 "
-      >
-        Company Statistics
-      </div>
+    <div className=" py-16 dark:bg-megamind_black lg:mt-[-80px]">
       <div>
         <AboutSection6 />
-       
       </div>
     </div>
   );
